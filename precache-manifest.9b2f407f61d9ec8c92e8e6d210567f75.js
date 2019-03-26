23cb8c6992d54b25556ae98fc7589b2b4b90eca2
Maniccia <mariamaniccia@gmail.com>
2019-03-26 14:05:21 -0400

    Updates

---
+++ precache-manifest.9b2f407f61d9ec8c92e8e6d210567f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96ed77558d1937bec54acdbb72b16e3",
+    "url": "/click-the-game/static/media/background.c96ed775.webp"
+  },
+  {
+    "revision": "96bd781c6ae5ae4141d4",
+    "url": "/click-the-game/static/js/runtime~main.7851883a.js"
+  },
+  {
+    "revision": "03194fafccd204e4e7d1",
+    "url": "/click-the-game/static/js/main.8a7d1756.chunk.js"
+  },
+  {
+    "revision": "74588f36c51fe9761d80",
+    "url": "/click-the-game/static/js/2.f31d23a6.chunk.js"
+  },
+  {
+    "revision": "03194fafccd204e4e7d1",
+    "url": "/click-the-game/static/css/main.9bdaf899.chunk.css"
+  },
+  {
+    "revision": "97d4cf1fe48dcb034aa62cc4baebc593",
+    "url": "/click-the-game/index.html"
+  }
+];
